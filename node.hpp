ea87e99c163a2fa428b8eb1c8bc26a5f5a0b8b6a
Yoni <yonatanyeshanov@gmail.com>
2016-04-21 00:44:35 +0300

    started adding "father" field

--- node.hpp
+++ node.hpp
@@ -20,18 +20,21 @@ namespace tree {
     class NODE {
         key* _key;
         value* _value;
+//        NODE<key,value>* _father;
         NODE<key,value>* _left;
 		NODE<key,value>* _right;
         
     public:
         NODE<key,value>(const key& k, const value& v,
-        		NODE* left = NULL, NODE* right = NULL) :
-        	_key(new key(k)), _value(new value(v)), _left(left), _right(right){}
+        		NODE* father = NULL, NODE* left = NULL, NODE* right = NULL) :
+        	_key(new key(k)), _value(new value(v)), /*_father(father),*/ _left(left), _right(right){}
         NODE<key,value>(NODE& node) = delete;
         ~NODE<key,value>(){ delete _key; delete _value; }
+//        void set_father(NODE* father) { _father = father; }
         void set_left(NODE* left) { _left = left; }
         void set_right(NODE* right) { _right = right; }
         void set_pair(const key& k, const value& v) { *(_key) = k; *(_value) = v; }
+//        NODE* get_father() { return _father; }
         NODE* get_left() { return _left; }
         NODE* get_right() { return _right; }
         key& get_key() { return *_key; }
